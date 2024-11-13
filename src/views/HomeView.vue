d1caf5bfa68460cc23752b3762ba1f795e7a5fa8
TimHi <TimHiller@live.de>
2024-11-13 01:47:32 +0100

    initial setup

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+import { onKeyStroke } from '@vueuse/core'
+import trumpetSfx from '../assets/monster.mp3'
+import { useSound } from '@vueuse/sound'
+const { play, stop } = useSound(trumpetSfx)
+onKeyStroke(['s', 'S', 'ArrowDown'], (e) => {
+  e.preventDefault()
+  play()
+})
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
